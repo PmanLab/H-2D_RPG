0029824655b4b62f10805f627647ce11764e722d
TanakaYukito <tanakayukito.business@gmail.com>
2024-11-14 12:16:47 +0900

    GameManagerを新規作成(FPS固定化メソッド等追加)

---
+++ Assets/Scripts/System/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //=== シリアライズ ===
+    [SerializeField, Header("FPS固定の設定値")] private int targetFPS = 60;
+
+    //=== インスタンス ===
+    public static GameManager instance;
+
+    /// <summary>
+    /// 第一初期化メソッド
+    /// </summary>
+    private void Awake()
+    {
+        //--- シングルトン ---
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    /// <summary>
+    /// 第二初期化メソッド
+    /// </summary>
+    private void Start()
+    {
+        // FPSを固定する
+        Application.targetFrameRate = targetFPS;
+    }
+
+}
