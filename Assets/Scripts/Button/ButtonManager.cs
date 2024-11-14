5781b0cfcaf3f4b4a242105df7f2554c81e2d83f
TanakaYukito <tanakayukito.business@gmail.com>
2024-11-14 12:25:59 +0900

    今後使うであろうButtonでのメソッドをまとめたButtonManagerを新規作成。

---
+++ Assets/Scripts/Button/ButtonManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+
+    /// <summary>
+    /// シーンロードメソッド 
+    /// </summary>
+    /// <param name="sceneName"></param>
+    public void LoadScene(string sceneName)
+    {
+        LoadScene(sceneName);
+    }
+
+    /// <summary>
+    /// アプリケーション終了メソッド
+    /// </summary>
+    public void EndButton()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    /// <summary>
+    /// オブジェクトActiveTrueメソッド
+    /// </summary>
+    /// <param name="gameObject"></param>
+    public void ShowGameObject(GameObject gameObject)
+    {
+        gameObject.SetActive(true);
+    }
+
+    /// <summary>
+    /// オブジェクトActiveFalseメソッド
+    /// </summary>
+    /// <param name="gameObject"></param>
+    public void HideGameObject(GameObject gameObject)
+    {
+        gameObject.SetActive(false);
+    }
+
+    /// <summary>
+    /// ポーズ解除専用メソッド
+    /// </summary>
+    public void UnPause()
+    {
+    }
+}
