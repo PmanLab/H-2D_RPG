c6d431791e7347fb119438c373fafdfd4fbc5573
TanakaYukito <tanakayukito.business@gmail.com>
2025-01-16 23:29:06 +0900

    お金をPlayerPrefsで管理(自動セーブするようにしました)

--- Assets/Scripts/Data/DataMoney.cs
+++ Assets/Scripts/Data/DataMoney.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Unity.VisualScripting;
+using UnityEngine;
 
 /// <summary>
 /// お金を保有するクラス
@@ -7,15 +8,34 @@
 [CreateAssetMenu(fileName = "NewGameData", menuName = "DataMoney/Money")]
 public class DataMoney : ScriptableObject
 {
-
     //=== シリアライズ ===
     [SerializeField, Header("初期所持金")]
     private int startingMoney = 100; // 初期所持金（デフォルト値：100）
 
+    //=== 変数宣言 ===
+    private const string MoneyKey = "PlayerMoney"; // PlayerPrefキー
+
     //=== プロパティ ===
     /// <summary>
-    /// 初期所持金を取得する
-    /// 外部からは値を変更できない
+    /// ・現在の所持金を取得、または設定する
+    /// ・PlayerPrefで現在の所持金値を保存
     /// </summary>
-    public int StartingMoney => startingMoney;
+    public int CurrentMoney
+    {
+        get => PlayerPrefs.GetInt(MoneyKey,startingMoney);  // デフォルト値を初期所持金に設定
+        set
+        {
+            PlayerPrefs.SetInt(MoneyKey, value);
+            PlayerPrefs.Save();
+        }
+    }
+
+    //=== メソッド ===
+    /// <summary>
+    /// ・現在のお金を初期所持金にリセットする
+    /// </summary>
+    public void ResertMoney()
+    {
+        CurrentMoney = startingMoney;
+    }
 }
