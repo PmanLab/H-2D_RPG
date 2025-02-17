2bd214ce1fce4495897e8a56b0565dafe589ffe4
TanakaYukito <tanakayukito.business@gmail.com>
2025-02-17 16:32:59 +0900

    オーディオ情報を管理するscriptを作成、それに基づきAudioMixerを新規作成

--- Assets/Scripts/Constant/ConstantManager.cs
+++ Assets/Scripts/Constant/ConstantManager.cs
@@ -1,6 +1,14 @@
-﻿public class ConstantManager
+﻿/// <summary>
+/// ・定義をまとめたクラス
+/// </summary>
+public class ConstantManager
 {
     // 定数
     public const int interactWaitingTime = 2;   // インタラクト待機時間
+
+    // ボリュームキー
+    public const string MasterVolumeKey = "MasterVolume";
+    public const string BGMVolumeKey = "BGMVolume";
+    public const string SEVolumeKey = "SEVolume";
 
 }
